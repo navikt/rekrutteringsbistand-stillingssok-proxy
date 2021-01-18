6ef3eec1b9d5327c024bd4c56f7a79b4585d4f21
Mads Lee Giil <mads.lee.giil@nav.no>
2021-01-18 14:22:29 +0100

    Sett opp CI/CD

--- src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/Main.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/Main.kt
@@ -1,5 +1,7 @@
 package no.nav.rekrutteringsbistand.stillingssokproxy
 
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -10,4 +12,13 @@ fun log(name: String): Logger = LoggerFactory.getLogger(name)
 
 fun main() {
     log("main").info("Starter applikasjon")
+
+    val port = System.getenv("JAVALIN_PORT").toInt()
+
+    val javalin = Javalin.create().start(7000)
+
+    javalin.routes {
+        get("/internal/isAlive") { it.status(200) }
+        get("/internal/isReady") { it.status(200) }
+    }
 }
