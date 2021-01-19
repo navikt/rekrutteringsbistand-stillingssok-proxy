1ef2d6864fef3f44ce32a2c29f2e827155f3e8d0
Joar Aurdal <joar.aurdal@nav.no>
2021-01-19 15:56:34 +0100

    Ta bort logging

--- src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/Security.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/Security.kt
@@ -17,10 +17,8 @@ class Security {
         javalin.before { context ->
             val url: String = context.req.requestURL.toString()
             val erÅpenUrl = tillateUrl.any { tillattUrl ->
-                log("Security").info("url1:${url} url2:${tillattUrl} tillatt:${url == tillattUrl}")
                 url == tillattUrl
             }
-            log("Security").info("sjekkurl:${url}")
 
             if (!erÅpenUrl) {
                 try {
