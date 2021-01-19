8ff1c1a6352f1b2d5c12aa1a96e0b09d13196bab
Joar Aurdal <joar.aurdal@nav.no>
2021-01-19 13:41:09 +0100

    Log url

--- src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/Security.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/Security.kt
@@ -17,6 +17,7 @@ class Security {
         javalin.before { context ->
             val url = context.req.requestURL
             val erÅpenUrl = tillateUrl.any { tillattUrl -> url.contains(tillattUrl) }
+            log.info("Security", "sjekkurl:${url}")
 
             if (!erÅpenUrl) {
                 val tokenValidationHandler = JwtTokenValidationHandler(getMultiIssuerConfiguration())
