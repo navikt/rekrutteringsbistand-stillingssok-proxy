3601eb210db876f9f9bb4f2d254a59a86c007b56
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-02-15 12:54:27 +0100

    Wrap main i try-catch med logging av exception

--- src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/Main.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/Main.kt
@@ -57,18 +57,23 @@ fun startApp(
 }
 
 fun main() {
-    val issuerProperties = when (environment["NAIS_CLUSTER_NAME"]) {
-        "dev-gcp" -> IssuerProperties(
-            URL("https://login.microsoftonline.com/NAVQ.onmicrosoft.com/.well-known/openid-configuration"),
-            listOf("38e07d31-659d-4595-939a-f18dce3446c5"),
-            issuer_isso
-        )
-        "prod-gcp" -> IssuerProperties(
-            URL("https://login.microsoftonline.com/navno.onmicrosoft.com/.well-known/openid-configuration"),
-            listOf("9b4e07a3-4f4c-4bab-b866-87f62dff480d"),
-            issuer_isso
-        )
-        else -> throw RuntimeException("Ukjent cluster")
+    try {
+        val issuerProperties = when (environment["NAIS_CLUSTER_NAME"]) {
+            "dev-gcp" -> IssuerProperties(
+                URL("https://login.microsoftonline.com/NAVQ.onmicrosoft.com/.well-known/openid-configuration"),
+                listOf("38e07d31-659d-4595-939a-f18dce3446c5"),
+                issuer_isso
+            )
+            "prod-gcp" -> IssuerProperties(
+                URL("https://login.microsoftonline.com/navno.onmicrosoft.com/.well-known/openid-configuration"),
+                listOf("9b4e07a3-4f4c-4bab-b866-87f62dff480d"),
+                issuer_isso
+            )
+            else -> throw RuntimeException("Ukjent cluster")
+        }
+        startApp(issuerProperties, ::lagSikkerhetsfilter)
+    } catch (e: Exception) {
+        log("main()").error("Noe galt skjedde", e)
+        throw e
     }
-    startApp(issuerProperties, ::lagSikkerhetsfilter)
 }
