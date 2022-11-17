d76377522c141e854aae71fe0e85738316dc2ccc
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-11-17 15:40:53 +0100

    Fiks logikk

--- src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/tokenValidationHandler.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/stillingssokproxy/tokenValidationHandler.kt
@@ -19,9 +19,11 @@ fun hentTokenValidationHandler(
     val issuerProperties = allIssuerProperties[rolle]!!
     val cachedHandler = cache[rolle]
 
-    return if (cachedHandler != null && cachedHandler.expires.isBefore(LocalDateTime.now())) {
+    return if (cachedHandler != null && cachedHandler.expires.isAfter(LocalDateTime.now())) {
+        log("hentTokenValidationHandler").info("Bruker cachet token validation handler, er cachet til ${cachedHandler.expires}")
         cachedHandler.handler
     } else {
+        log("hentTokenValidationHandler").info("Henter og cacher ny token validation handler")
         val newHandler = JwtTokenValidationHandler(
             MultiIssuerConfiguration(mapOf(issuerProperties.cookieName to issuerProperties))
         )
