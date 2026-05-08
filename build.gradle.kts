c5ad31eb900b909b979304b9feb6bd59d540c226
Are Husby <are.husby@nav.no>
2026-05-08 15:03:01 +0200

    Bruk bom for å synce versjonsnummer mellom artefakter

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("io.micrometer:micrometer-bom:1.15.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+
     implementation(kotlin("stdlib"))
     implementation("ch.qos.logback:logback-classic:1.5.16")
     implementation("io.github.cdimascio:dotenv-kotlin:6.5.0")
@@ -31,16 +34,16 @@ dependencies {
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     implementation("no.nav.security:token-validation-core:5.0.14")
     implementation("org.opensearch.client:opensearch-rest-high-level-client:2.18.0")
-    implementation("io.micrometer:micrometer-core:1.15.0")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.15.0")
+    implementation("io.micrometer:micrometer-core")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("io.prometheus:simpleclient_common:0.16.0")
 
     testImplementation("com.github.kittinunf.fuel:fuel:2.3.1")
     testImplementation("com.github.kittinunf.fuel:fuel-jackson:2.3.1")
     testImplementation("no.nav.security:mock-oauth2-server:2.1.0")
     testImplementation("org.assertj:assertj-core:3.27.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
     testImplementation("org.mock-server:mockserver-netty:5.15.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
